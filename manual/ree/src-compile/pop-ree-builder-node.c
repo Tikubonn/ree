0e3b4b887a639752c5f7898d5cda18518e231a89
tikubonn <https://twitter.com/tikubonn>
2018-08-24 13:29:14 +0900

    rewrote some codes about builder modification.

--- manual/ree/src-compile/pop-ree-builder-node.c
+++ manual/ree/src-compile/pop-ree-builder-node.c
@@ -1,9 +1,30 @@
 #include <ree.h>
 
+/* I will rewrite here XD */
+
 int pop_ree_builder_node (ree_builder *builder, ree_node **node){
-  if (builder->node == NULL)
+
+	ree_node *nda = builder->node;
+	if (nda == NULL)
+		return 1;
+	
+	ree_node *ndb = nda->previous;
+	if (ndb != NULL)
+		ndb->next = NULL;
+	nda->previous = NULL;
+	
+	*node = nda;
+	builder->node = ndb;
+	
+	if (builder->root == nda)
+		builder->root = NULL;
+	
+	return 0;
+	
+	/* if (builder->node == NULL)
     return 1;
   *node = builder->node;
   builder->node = previous_ree_node(builder->node);
-  return 0;
+  return 0; */
+	
 }
