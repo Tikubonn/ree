58c949b814984e66a33148684f0921bdb2abf71f
tikubonn <https://twitter.com/tikubonn>
2018-08-25 22:07:55 +0900

    added some test codes.

--- test.c
+++ test.c
@@ -67,6 +67,65 @@ free_ree(ree);\
 free_ree_string(source2);\
 }
 
+#define test3(regexp, text, beg1, ed1, beg2, ed2) {\
+ree_string *source = make_ree_string0(regexp);\
+ree *ree = make_ree(source);\
+int status1 = compile_ree(ree);\
+if (status1){\
+fprintf(stderr, "line of %lu: compile_ree() caused error! (%d)\n", __LINE__, status1);\
+abort();\
+}\
+bool found;\
+ree_string *source2 = make_ree_string0(text);\
+int status2 = match_ree(source2, ree, &found);\
+if (status2){\
+fprintf(stderr, "line of %lu: match_ree() caused error! (%d)\n", __LINE__, status1);\
+abort();\
+}\
+if (found == false){\
+dump_ree(stderr, ree);\
+fprintf(stderr, "failed: \"%s\" <= \"%s\" unmatched.\n", regexp, text);\
+abort();\
+}\
+ree_region region1;\
+ree_region region2;\
+int status3 = get_ree_group(0, ree, &region1);\
+if (status3){\
+dump_ree(stderr, ree);\
+fprintf(stderr, "line of %lu: get_ree_group() caused error! (%d)\n", __LINE__, status3);\
+}\
+int status4 = get_ree_group(1, ree, &region2);\
+if (status4){\
+dump_ree(stderr, ree);\
+fprintf(stderr, "line of %lu: get_ree_group() caused error! (%d)\n", __LINE__, status4);\
+abort();\
+}\
+if (region1.beginning != beg1){\
+dump_ree(stderr, ree);\
+fprintf(stderr, "failed: \"%s\" <= \"%s\" (%d != %d) matched group1 beginning position was unequal.\n", regexp, text, region1.beginning, region1.end);\
+abort();\
+}\
+if (region1.end != ed1){\
+dump_ree(stderr, ree);\
+fprintf(stderr, "failed: \"%s\" <= \"%s\" (%d != %d) matched group1 end position was unequal.\n", regexp, text, region1.beginning, region1.end);\
+abort();\
+}\
+if (region2.beginning != beg2){\
+dump_ree(stderr, ree);\
+fprintf(stderr, "failed: \"%s\" <= \"%s\" (%d != %d) matched group2 beginning position was unequal.\n", regexp, text, region2.beginning, region2.end);\
+abort();\
+}\
+if (region2.end != ed2){\
+dump_ree(stderr, ree);\
+fprintf(stderr, "failed: \"%s\" <= \"%s\" (%d != %d) matched group2 end position was unequal.\n", regexp, text, region2.beginning, region2.end);\
+abort();\
+}\
+fprintf(stdout, "success: \"%s\" <= \"%s\" (%d, %d) (%d, %d) = true\n", regexp, text, region1.beginning, region1.end, region2.beginning, region2.end);\
+free_ree_string(source);\
+free_ree(ree);\
+free_ree_string(source2);\
+}
+
 int main (){
   
   test1("mo..!", "moco!", true);
@@ -168,6 +227,36 @@ int main (){
 	test1("[<aaaa>-<zzzz>]", "zzzz", true);
 	test1("[<aaaa>-<zzzz>]", "aa00", true);
 	test1("[<aaaa>-<zzzz>]", "00aa", false);
+	
+	test3("(<mo>*)(<co>*)", "moco", 0, 2, 2, 4);
+	test3("(<mo>*)(<co>*)", "momoco", 0, 4, 4, 6);
+	test3("(<mo>*(<co>*))", "moco", 0, 4, 2, 4);
+	test3("(<mo>*(<co>*))", "momoco", 0, 6, 4, 6);
+	
+	test3("(<mo>+)(<co>+)", "moco", 0, 2, 2, 4);
+	test3("(<mo>+)(<co>+)", "momoco", 0, 4, 4, 6);
+	test3("(<mo>+(<co>+))", "moco", 0, 4, 2, 4);
+	test3("(<mo>+(<co>+))", "momoco", 0, 6, 4, 6);
+	
+	test3("(<mo>{1,3})(<co>{1,3})", "moco", 0, 2, 2, 4);
+	test3("(<mo>{1,3})(<co>{1,3})", "momoco", 0, 4, 4, 6);
+	test3("(<mo>{1,3}(<co>{1,3}))", "moco", 0, 4, 2, 4);
+	test3("(<mo>{1,3}(<co>{1,3}))", "momoco", 0, 6, 4, 6);
+	
+	test3("(<mo>*)(<co>*)", "moco....", 0, 2, 2, 4);
+	test3("(<mo>*)(<co>*)", "momoco......", 0, 4, 4, 6);
+	test3("(<mo>*(<co>*))", "moco....", 0, 4, 2, 4);
+	test3("(<mo>*(<co>*))", "momoco......", 0, 6, 4, 6);
+	
+	test3("(<mo>+)(<co>+)", "moco....", 0, 2, 2, 4);
+	test3("(<mo>+)(<co>+)", "momoco......", 0, 4, 4, 6);
+	test3("(<mo>+(<co>+))", "moco....", 0, 4, 2, 4);
+	test3("(<mo>+(<co>+))", "momoco......", 0, 6, 4, 6);
+	
+	test3("(<mo>{1,3})(<co>{1,3})", "moco....", 0, 2, 2, 4);
+	test3("(<mo>{1,3})(<co>{1,3})", "momoco......", 0, 4, 4, 6);
+	test3("(<mo>{1,3}(<co>{1,3}))", "moco....", 0, 4, 2, 4);
+	test3("(<mo>{1,3}(<co>{1,3}))", "momoco......", 0, 6, 4, 6);
 	
   return 0;
   
